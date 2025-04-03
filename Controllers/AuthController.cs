c127e6e9787b52ab924edbac7fc96d1995aadc9f
Halfdan <halfdansc@gmail.com>
2025-04-03 10:37:19 +0200

    started the AuthController.cs

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MuAuthApp.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        // login endpoint as well as generate JSON web token (JWT)
+        // this is a POST request where we accept a username and password in the request body
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var token = GenerateJwtToken(user);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized();
+        }
+
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var claims = new []
+            {
+                // Sub is the subject of the token (the username of the authenticated user)
+                // Jti is a unique identifier for the token aka GUID
+                new Claim(JwtRegisteredClaimNames.Sub, user.userName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), 
+            };
+            
+            // we use the secret key from appsettins to sing the jwt
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["ExpireMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
